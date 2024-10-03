2adb788817b6bd18a51ca9ca62af79cb0d38a242
AlissonAzu <alisson.grings@gmail.com>
2024-10-02 21:43:25 -0300

    Atualizando lista 3

---
+++ lista 3/exercicio 14.c
@@ -0,0 +1,27 @@
+/*
+Criar um programa para identificar o valor a ser pago por um plano de saúde dada a idade do 
+conveniado considerando que todos pagam R$ 300 e mais um adicional (se tiver dependentes) 
+conforme a seguinte tabela:
+a) crianças com menos de 10 anos pagam R$100;
+b) dependentes com idade entre 10 e 30 anos pagam R$220;
+c) dependentes com idade entre 31 e 60 anos pagam R$ 395; e
+d) dependentes com mais de 60 anos pagam R$ 480
+*/
+
+#include <stdio.h>
+
+int main ()
+{
+    int crincas, adultos, meiaIdade, idosos, total, opcao;
+    printf("Deseja abrir um plano de saúde?\n");
+    printf("1 - Sim\n");
+    printf("2 - Não\n");
+    scanf("%d", &opcao);
+    
+    if(opcao == 1){
+
+    }
+
+
+    return 0;
+}
